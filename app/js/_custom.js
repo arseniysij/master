0f22b9933ea54ee7440196e88b03e7454d33d907
arseniysij <50396694+arseniysij@users.noreply.github.com>
2020-07-06 08:25:56 +0500

    more

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,13 +1,69 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	  Modernizr.on('webp', function (result) {
-    if (result) {
-      // supported
-    } else {
-      // not-supported
-    }
-  });
+	  
 
 	$('.select-occupation').niceSelect();
+  //paralax mouse
+  
+  var scene = document.querySelector('.program__paralax');
+var parallaxInstance = new Parallax(scene, {
+  // relativeInput: true
+});
 
+// animate css
+// function callbackFunc(entries, observer)
+// {
+//   entries.forEach(entry => {
+//     var txt = entry.target.id + " visibility: " + entry.isIntersecting;
+    
+//     document.getElementById('log').appendChild(document.createTextNode(txt));
+//     document.getElementById('log').appendChild(document.createElement("br"));
+//   });
+// }
+
+// let options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.3
+//   };
+
+// let observer = new IntersectionObserver(callbackFunc, options);
+
+// observer.observe(document.querySelector('firstBlock'));
+// observer.observe(document.getElementById('secondBlock'));
+
+
+
+// Change date in hero
+  let dateOnPage = $('.date__bold'),
+  monthOnPage = $('.date__month');
+  let now = new Date();
+  let userDay = now.getDate();
+  let userHours = now.getHours();
+
+  function getMonth(date) {
+    let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    return months[date.getMonth()];
+  }
+
+  $(monthOnPage).text(getMonth(now));
+  $(dateOnPage).text(userDay);
+  if (userHours >= 19) {
+    (dateOnPage).text(userDay+1);
+  }
+
+  // form__author hover
+  let formAuthor = $('.form__author');
+  let desc = $('.author__decs');
+
+  formAuthor.on('mouseleave', function() {
+    desc.fadeOut(200);   
+  });
+   
+  formAuthor.on('mouseenter', function() {
+    desc.fadeIn(200);
+  });
+
+  
+  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -47,8 +47,10 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
 		'app/js/_jquery.nice-select.min.js', // custom select
+		'app/js/_parallax.min.js', // paralax on mouse
+		'app/js/_wow.min.js', // paralax on mouse
 		// 'app/js/_modernizr-custom.js', // bg webp
-		// 'app/js/_libs.js', // JS libraries (all in one)
+		'app/js/_libs.js', // JS libraries (all in one)
 		'app/js/_custom.js', // Custom scripts. Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
