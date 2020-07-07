bb4dbe41965edb27be604010cb4abc8ac42e5d4b
arseniysij <50396694+arseniysij@users.noreply.github.com>
2020-07-07 19:56:28 +0500

    video

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -12,56 +12,84 @@ document.addEventListener("DOMContentLoaded", function() {
       // once: true
     });
   
+  // video play
+  let videoWrap = $('.video-wrap'),
+  videoContainer = videoWrap.find('.video-container');
+  let videoClose = videoWrap.find('.video-close');
+  
+  $('.play-btn').click(function(e) {
+    e.preventDefault();
+    $('body').css('overflow', 'hidden');
+    videoWrap.fadeIn(200);
+    videoContainer.html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/vs5k_wLFaH4" frameborder="0" allow="accelerometer"; autoplay="1"; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    
+    videoClose.click(function() {
+      videoContainer.html('');
+      videoWrap.fadeOut(200);
+      $('body').css('overflow', '');
+    });
+  });
+
+ 
 
 	$('.select-occupation').niceSelect();
 
   //paralax mouse
-  if($('.program__paralax')) {
-    var scene = document.querySelector('.program__paralax');
-var parallaxInstance = new Parallax(scene, {
-  // relativeInput: true
-});
-
-  }
-  
-
-  
-
-// animate css
-// function callbackFunc(entries, observer)
-// {
-//   entries.forEach(entry => {
-//     var txt = entry.target.id + " visibility: " + entry.isIntersecting;
     
-//     document.getElementById('log').appendChild(document.createTextNode(txt));
-//     document.getElementById('log').appendChild(document.createElement("br"));
-//   });
-// }
+      if($('.program__paralax')) {
+      var scene = document.querySelector('.program__paralax');
+  var parallaxInstance = new Parallax(scene, {
+    // relativeInput: true
+  });
+  }
+    
+ 
+ 
+  
+  var scene2 = document.querySelector('.audience__mockup');
+  var parallaxInstance2 = new Parallax(scene2);
+    
+    
+  
 
-// let options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.3
-//   };
 
-// let observer = new IntersectionObserver(callbackFunc, options);
+  
 
-// observer.observe(document.querySelector('firstBlock'));
-// observer.observe(document.getElementById('secondBlock'));
+  // animate css
+  // function callbackFunc(entries, observer)
+  // {
+  //   entries.forEach(entry => {
+  //     var txt = entry.target.id + " visibility: " + entry.isIntersecting;
+      
+  //     document.getElementById('log').appendChild(document.createTextNode(txt));
+  //     document.getElementById('log').appendChild(document.createElement("br"));
+  //   });
+  // }
+
+  // let options = {
+  //     root: null,
+  //     rootMargin: '0px',
+  //     threshold: 0.3
+  //   };
+
+  // let observer = new IntersectionObserver(callbackFunc, options);
+
+  // observer.observe(document.querySelector('firstBlock'));
+  // observer.observe(document.getElementById('secondBlock'));
 
 
 
-// Change date in hero
+  // Change date in hero
   let dateOnPage = $('.date__bold'),
   monthOnPage = $('.date__month');
   let now = new Date();
   let userDay = now.getDate();
   let userHours = now.getHours();
 
-  function getMonth(date) {
-    let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-    return months[date.getMonth()];
-  }
+    function getMonth(date) {
+      let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+      return months[date.getMonth()];
+    }
 
   $(monthOnPage).text(getMonth(now));
   $(dateOnPage).text(userDay);
